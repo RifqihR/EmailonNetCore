a1a2904e22edd3da1ff97ee87763eaa6720f541b
RifqiHanif <rifqihanif24@gmail.com>
2023-03-21 11:05:36 +0700

    adding sending email

---
+++ EmailonNetCore/Controllers/EmailController.cs
@@ -0,0 +1,35 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace EmailonNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("zora.ratke@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("zora.ratke@ethereal.email"));
+            email.Subject = "Test Email Subject";
+            email.Body = new TextPart(TextFormat.Html)
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email", 587,SecureSocketOptions.StartTls);
+            smtp.Authenticate("zora.ratke@ethereal.email", "ZdGbCHZChHZ9AVycy9");
+            smtp.Send(email);
+            smtp.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
